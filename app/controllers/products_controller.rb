6daec1db0ab479d5d7b567136232afed5cf5aee7
Nachiket Pusalkar <nachiket87@gmail.com>
2021-01-13 19:24:52 -0500

    Add input form and post

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,8 +4,23 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
   end
 
   def create
+    product = Product.new(product_params)
+    respond_to do |format|
+      if product.save
+        format.html {redirect_to products_path, notice: "product saved!"}
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :rating, :price, :photo)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ApplicationRecord
   validates :rating, numericality: { only_integer: true, less_than_or_equal_to: 5, greater_than_or_equal_to: 0 }
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :photo, presence: true
   attribute :in_card, :boolean, default: false
   has_one_attached :photo
 end
