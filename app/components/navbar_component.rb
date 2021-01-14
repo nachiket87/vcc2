042524a80ddc89fbf5f0759b37be84b9f62f3359
Nachiket Pusalkar <nachiket87@gmail.com>
2021-01-13 20:12:19 -0500

    Fix styling

--- app/components/navbar_component.rb
+++ app/components/navbar_component.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class NavbarComponent < ViewComponent::Base
+
+  private 
+
+  def on_index_page?
+    current_page?(controller: 'products', action: 'index')
+  end
 end
